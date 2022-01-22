8cc29d189d3f62614d67e093744e4fc888d8266d
matthewhirai <mhirai301@gmail.com>
2022-01-21 19:21:47 -0800

    removed most song replace if statements, moved includes 'ou' to fetchTracks
    - replacing 'ou' only happens if the word is non-english

--- src/spotify.js
+++ src/spotify.js
@@ -17,137 +17,7 @@ function fetchTracks(song) {
 	let title = song.split(' by ')[0];
 	let artist = song.split(' by ')[1];
 
-	if (title.includes('&nbsp;')) {
-		title = title.replace('&nbsp;', '');
-	}
-
-	if (artist.includes('&nbsp;')) {
-		artist = artist.replace('&nbsp;', '');
-	}
-
-	if (artist.includes('&amp;')) {
-		artist = artist.replace('&amp;', '&');
-	}
-
-	if (artist.includes('feat.')) {
-		artist = artist.substring(0, artist.indexOf('feat.'));
-		artist = artist.trim();
-	}
-
-	// Adachi to Shimamura
-	if (title.includes('Kimi no Tonari de')) {
-		title = 'Kimi no Tonaride (キミのとなりで)';
-	}
-
-	if (title.includes('Kimi ni Aeta Hi (君に会えた日)')) {
-		title = 'Kimi ni Aetahi';
-		artist = 'Adachi(CV:Akari Kito)';
-	}
-
-	// Tonikaku Kawaii
-	if (title == 'Koi no Uta') {
-		title = 'Koino Uta';
-	}
-
-	// Anohana
-	if (title.includes('secret base ~Kimi ga Kureta Mono~ (10 years after ver.)')) {
-		title = 'secret base ~君がくれたもの~ (10 years after Ver.)';
-		artist = '本間芽衣子 (CV.茅野愛衣)';
-	}
-
-	// Code Geass R2
-	if (title.includes('Shiawase Neiro (ｼｱﾜｾﾈｲﾛ)')) {
-		title = 'Shiawase Neiro (シアワセネイロ)';
-	}
-
-	if (title.includes('02~O-Two~ (02~ｵｰ･ﾂｰ~)')) {
-		title = 'O2';
-	}
-
-	// Vivy: Fluorite Eye's Song
-	if (song == 'Galaxy Anthem by Vivy (Kairi Yagi)') {
-		artist = 'ディーヴァ(Vo.八木海莉)';
-	}
-
-	if (title == "Flourite Eye's Song") {
-		title = "Fluorite Eye's Song";
-	}
-
-	if (artist.includes('Vivy')) {
-		artist = 'ヴィヴィ(Vo.八木海莉)';
-	}
-
-	if (artist.includes('Estella')) {
-		artist = 'エステラ(Vo.六花)';
-	}
-
-	if (artist == 'General-Purpose Diva AI (Miya Kotsuki)') {
-		artist = '汎用型歌姫AI(Vo.コツキミヤ)';
-	}
-
-	// Love is War S1/S2
-	if (title == 'Chikatto Chika Chika♡ (チカっとチカ千花っ♡)') {
-		title = 'Chikatto Chika Chika';
-		artist = 'Konomi Kohara';
-	}
-
-	if (artist == 'Haruka Fukuhara') {
-		artist = '福原 遥';
-	}
-
-	// AOT S3
-	if (title.includes('Akatsuki no Chinkonka')) {
-		title = 'Akatsuki no Requiem (暁の鎮魂歌)';
-	}
-
-	// Houkago Teibou Nisshi
-	if (artist.includes('Umino High School Teibou-bu')) {
-		artist = '海野高校ていぼう部:鶴木陽渚(CV:高尾奏音), 帆高夏海(CV:川井田夏海),黒岩悠希(CV:篠原侑),大野真(CV:明坂聡美)';
-	}
-
-	// The End Girl Trip
-	if (artist.includes('Chito (Inori Minase) & Yuri (Yurika Kubo)')) {
-		artist = 'チト(CV:水瀬いのり) & ユーリ(CV:久保ユリカ)';
-	}
-
-	// Yuruyuri
-	if (artist.includes('Nanamori Chu☆')) {
-		artist = 'Nanamori-Chu☆Goraku-Bu';
-	}
-
-	if (artist.includes('Yui Funami (Minami Tsuda) & Chinatsu Yoshikawa (Rumi Ookubo)')) {
-		artist = '結衣(CV:津田美波) & "ちなつ(CV:大久保瑠美)"';
-	}
-
-	// Oregairu
-	if (artist === 'Nagi Yanagi') {
-		artist = '"yanaginagi"';
-	}
-
-	if (artist.includes('Yukino Yukinoshita (Saori Hayami)')) {
-		artist = '雪ノ下雪乃(CV.早見沙織)';
-	}
-
-	if (artist.includes('Yui Yuigahama (Nao Touyama)')) {
-		artist = '由比ヶ浜結衣(CV.東山奈央)';
-	}
-
-	// Takagi-san
-	if (song == 'Zero Centimeter by Yuiko Oohara') {
-		title = 'ゼロセンチメートル ';
-	}
-
-	if (title.includes('(奏（かなで）)')) {
-		title = title.replace('(奏（かなで）)', '"奏(かなで)"');
-	}
-
-	if (artist == 'Yuiko Oohara') {
-		artist = '大原ゆい子';
-	}
-
-	if (artist.includes('Takagi-san')) {
-		artist = '高木さん(CV:高橋李依)';
-	}
+	[title, artist] = deleteFromSong(title, artist, song);
 
 	originalTitle = title;
 	originalArtist = artist;
@@ -166,19 +36,22 @@ function fetchTracks(song) {
 		title = title.replace('#', '');
 	}
 
-	// Check if there is an ou in the title and set the english title to that
-	if (originalTitle.includes('ou') && !originalTitle.includes('Shoukei to Shikabane no Michi')) {
-		originalTitle = originalTitle.replace('ou', 'o');
+	// Check if there is an ou in the title and remove it
+	if (originalTitle.includes('ou')) {
 		if (originalTitle.includes('(')) {
 			englishTitle = originalTitle.split(' (')[0];
 		} else {
-			title = originalTitle; // if there is no translation then set the title to new originalTitle w/o 'ou'
+			let substringOU = originalTitle.split(' ').find((a) => a.includes('ou'));
+			fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${substringOU}`)
+				.then((response) => {
+					return response.json();
+				})
+				.then((data) => {
+					if (!data.message) {
+						title = title.replace('ou', 'o'); // if there is no translation then set the title to new originalTitle w/o 'ou'
+					}
+				});
 		}
-	}
-
-	if (originalArtist.includes('ou')) {
-		originalArtist = originalArtist.replace('ou', 'o');
-		artist = originalArtist;
 	}
 
 	// remove feat from title
@@ -221,12 +94,12 @@ function handleTrackResponse() {
 		let valid = false;
 		let url = '';
 		let artistsWithoutUU = '@&';
+		let artistsWithoutOU = '@&';
 		let dict = {}; //all artists with track title
 
 		//title
 		for (let i = 0; i < data.tracks.items.length; i++) {
-			if (
-				(data.tracks.items[i].name.toLowerCase().includes(originalTitle.toLowerCase()) ||
+			if ((data.tracks.items[i].name.toLowerCase().includes(originalTitle.toLowerCase()) ||
 					data.tracks.items[i].name.toLowerCase().includes(englishTitle.toLowerCase()) ||
 					data.tracks.items[i].name.includes(translationTitle.substring(0, 2))) &&
 				!data.tracks.items[i].name.toLowerCase().includes('tv size')
@@ -240,19 +113,20 @@ function handleTrackResponse() {
 			artistsWithoutUU = originalArtist.slice(0, index) + originalArtist.slice(index + 1);
 		}
 
+		if (originalArtist.includes('ou')) {
+			let index = originalArtist.indexOf('ou');
+			artistsWithoutOU = originalArtist.slice(0, index) + originalArtist.slice(index + 1);
+		}
+
 		//artist
 		outerLoop: for (const [key, value] of Object.entries(dict)) {
 			for (let i = 0; i < value.length; i++) {
 				let spotifyName = value[i].name.toLowerCase();
-				if (
-					spotifyName.includes(originalArtist.toLowerCase()) ||
-					originalArtist.toLowerCase().includes(spotifyName) ||
-					spotifyName.includes(translationArtist.toLowerCase()) ||
-					translationArtist.toLowerCase().includes(spotifyName) ||
-					spotifyName.includes(englishArtist.toLowerCase()) ||
-					englishArtist.toLowerCase().includes(spotifyName) ||
-					spotifyName.includes(artistsWithoutUU.toLowerCase()) ||
-					artistsWithoutUU.toLowerCase().includes(spotifyName)
+				if (spotifyName.includes(originalArtist.toLowerCase()) || originalArtist.toLowerCase().includes(spotifyName) ||
+					spotifyName.includes(translationArtist.toLowerCase()) || translationArtist.toLowerCase().includes(spotifyName) ||
+					spotifyName.includes(englishArtist.toLowerCase()) || englishArtist.toLowerCase().includes(spotifyName) ||
+					spotifyName.includes(artistsWithoutUU.toLowerCase()) || artistsWithoutUU.toLowerCase().includes(spotifyName) ||
+					spotifyName.includes(artistsWithoutOU.toLowerCase()) || artistsWithoutOU.toLowerCase().includes(spotifyName)
 				) {
 					url = data.tracks.items[key].external_urls.spotify;
 					addTrack(url);
@@ -260,10 +134,8 @@ function handleTrackResponse() {
 					break outerLoop;
 				} else if (translationArtist.length > 0) {
 					// Check if there is a japanese translation
-					if (
-						japanese.test(translationArtist) === true &&
-						translationArtist.includes(value[i].name) &&
-						japanese.test(value[i].name) === true
+					if ( japanese.test(translationArtist) === true && translationArtist.includes(value[i].name) 
+						&& japanese.test(value[i].name) === true
 					) {
 						url = data.tracks.items[key].external_urls.spotify;
 						addTrack(url);
@@ -278,7 +150,7 @@ function handleTrackResponse() {
 			if (count != 0) {
 				count = 0;
 			} else if (englishTitle != '@&' && englishTitle.length > 0) {
-				url = SEARCH + `?q=${englishTitle}&type=track`;
+				url = SEARCH + `?q=${englishTitle}&type=track&limit=50`;
 				count++;
 				callApi('GET', url, null, handleTrackResponse);
 			}
